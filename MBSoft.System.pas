3c4fae12d5836c2c59ec2775b9a245643e637b1e
MarcoBridda <marcobridda.mbsoft@gmail.com>
2024-03-29 10:29:00 +0100

    MBSoft.System versione 1.2.0.0

--- MBSoft.System.pas
+++ MBSoft.System.pas
@@ -121,7 +121,7 @@ type
   end;
 
 const
-  LIB_VERSION: TProductFileVersion = (Major: 1; Minor: 1; Build: 0; Release: 0);
+  LIB_VERSION: TProductFileVersion = (Major: 1; Minor: 2; Build: 0; Release: 0);
 
 implementation
 
