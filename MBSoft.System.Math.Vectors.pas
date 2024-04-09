82752f2c0a624633e74f8579628ee0dc0dd3a4ca
MarcoBridda <marcobridda.mbsoft@gmail.com>
2024-04-09 16:26:30 +0200

    Aggiunta unit MBSoft.System.Math.Vectors

---
+++ MBSoft.System.Math.Vectors.pas
@@ -0,0 +1,15 @@
+unit MBSoft.System.Math.Vectors;
+//******************************************************************************
+//Aggiunte alla Unit System.Math.Vectors
+//
+//Copyright MBSoft(2024)
+//******************************************************************************
+
+interface
+
+uses
+  System.Math.Vectors;
+
+implementation
+
+end.
